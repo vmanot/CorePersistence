d882e2c066638ca8d4f58db561abb1fd6fc2e56c
Vatsal Manot <vatsal.manot@yahoo.com>
2024-04-10 00:56:07 -0700

    Update package

--- Sources/_JSON/JSON Schema/JSONSchema-Initializers.swift
+++ Sources/_JSON/JSON Schema/JSONSchema-Initializers.swift
@@ -5,7 +5,25 @@
 import Swift
 
 extension JSONSchema {
+    public static var integer: JSONSchema {
+        JSONSchema(type: .integer)
+    }
+    
+    public static var number: JSONSchema {
+        JSONSchema(type: .number)
+    }
+
+    public static var string: JSONSchema {
+        JSONSchema(type: .string)
+    }
+        
     public static func array(_ schema: JSONSchema) -> JSONSchema {
         JSONSchema(type: .array, items: schema)
+    }
+    
+    public static func object(
+        properties: [String: JSONSchema]
+    ) -> JSONSchema {
+        JSONSchema(type: .object, properties: properties)
     }
 }
